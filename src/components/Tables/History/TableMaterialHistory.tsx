97483220948b0a8f2c8518a7d3cf620ef9572b30
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-03-28 12:56:06 +0700

    Update : Button Diffferent

--- src/components/Tables/History/TableMaterialHistory.tsx
+++ src/components/Tables/History/TableMaterialHistory.tsx
@@ -84,6 +84,8 @@ const TableMatHistory = () => {
   };
 
   const closeModal2 = () => setShowModal2(false);
+
+
   return (
     <div className="rounded-sm border border-stroke rounded-b-xl bg-white px-5 pt-2 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
 
@@ -223,6 +225,7 @@ const TableMatHistory = () => {
 
               </td>
             </div>
+
           </div>
         ))}
       </div>
