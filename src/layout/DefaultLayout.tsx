3a2871123746b023b47dfacac2eb2552678abd7e
Tio Noviar <-g>
2024-04-17 15:16:44 +0700

    size adjust

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -21,7 +21,7 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            <div className="mx-auto max-w-screen-2xl px-4 pb-4 md:px-6 md:pb-6 2xl:px-10 2xl:pb-10">
               {children}
             </div>
           </main>
