0b9d93a903e494db6a3fb9c81788541efe64fe37
Tio Noviar <-g>
2024-05-13 15:09:57 +0700

    page

---
+++ src/pages/inspection/os3/Preventive.tsx
@@ -0,0 +1,20 @@
+
+import DefaultLayout from '../../../layout/DefaultLayout';
+
+import Os3FullWidthTabs from './Os3FullWidthTabs';
+
+
+const Preventive = () => {
+
+    return (
+        <DefaultLayout>
+
+            <p className='font-semibold md:text-[28px] text-[20px] text-primary mb-[18px]'>Maintenance &gt; OS 3</p>
+            <Os3FullWidthTabs />
+
+
+        </DefaultLayout>
+    );
+};
+
+export default Preventive;
