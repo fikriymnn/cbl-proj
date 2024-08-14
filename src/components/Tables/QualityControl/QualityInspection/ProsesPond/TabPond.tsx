579866934253c31b96ba0f863c2ecb573c04a396
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-08-14 13:49:13 +0700

    update

--- src/components/Tables/QualityControl/QualityInspection/ProsesPond/TabPond.tsx
+++ src/components/Tables/QualityControl/QualityInspection/ProsesPond/TabPond.tsx
@@ -7,6 +7,7 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProsesPondMesin from './ProsesPondMesin';
+import ProsesPondMesinHistory from './ProsesPondMesinHistory';
 
 
 
@@ -167,7 +168,7 @@ export default function qTabPond() {
                     <ProsesPondMesin />
                 </TabPanel>
                 <TabPanel value={value} index={1} dir={theme.direction}>
-
+                    <ProsesPondMesinHistory />
                 </TabPanel>
 
             </Box>
