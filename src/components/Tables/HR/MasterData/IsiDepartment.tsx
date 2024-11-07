5fe53eb449fcf8ed6229431b0134616772e84ae3
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-11-07 22:31:12 +0700

    update

--- src/components/Tables/HR/MasterData/IsiDepartment.tsx
+++ src/components/Tables/HR/MasterData/IsiDepartment.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import ModalKosonganSmall from '../../../Modals/ModalKosonganSmall';
 
 function IsiDepartment() {
 
@@ -31,42 +32,97 @@ function IsiDepartment() {
         }
     }
 
-    const masterKaryawan = [
-        {
-            nik: 123,
-            nama: 'asep kurma',
-            jenis_kelamin: 'L',
-            posisi: 'Tetap',
-            divisi: 'quality',
-            tipe_penggajian: 'Bulanan',
-            tanggal_masuk: '10/11/2024',
-            tanggal_keluar: '',
-            status_data: 'Contact'
-        },
-        {
-            nik: 124,
-            nama: 'kurma aspe',
-            jenis_kelamin: 'L',
-            posisi: 'Tetap',
-            divisi: 'quality',
-            tipe_penggajian: 'Bulanan',
-            tanggal_masuk: '11/11/2024',
-            tanggal_keluar: '',
-            status_data: 'Pendidikan'
-        },
-        {
-            nik: 125,
-            nama: 'kurma aspear',
-            jenis_kelamin: 'L',
-            posisi: 'Tetap',
-            divisi: 'quality',
-            tipe_penggajian: 'Bulanan',
-            tanggal_masuk: '11/11/2024',
-            tanggal_keluar: '11/11/2024',
-            status_data: 'Complete'
-        }
-    ]
+    const [showHistory, setShowHistory] = useState(false);
+    const openModalHistory = () => setShowHistory(true);
+    const closeModalHistory = () => setShowHistory(false);
 
+    const [namaDepartment, setnamaDepartment] = useState<any>();
+    async function postMasterMesin() {
+
+        const url = `${import.meta.env.VITE_API_LINK}/master/hr/department`;
+        try {
+            const res = await axios.post(url,
+                {
+                    nama_department: namaDepartment
+
+                },
+                {
+
+                    withCredentials: true,
+                });
+            window.location.reload();
+            getKaryawan()
+            console.log(res.data);
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+
+    const [showEdit, setShowEdit] = useState<any>([]);
+    const openEdit = (i: any) => {
+        const onchangeVal: any = [...showEdit];
+        onchangeVal[i] = true;
+
+        setShowEdit(onchangeVal);
+    };
+    const closeEdit = (i: any) => {
+        const onchangeVal: any = [...showEdit];
+        onchangeVal[i] = false;
+
+        setShowEdit(onchangeVal);
+    };
+
+    const [namaDepartmentEdit, setnamaDepartmentEdit] = useState<any>();
+    async function editMasterMesin(id: number) {
+
+        const url = `${import.meta.env.VITE_API_LINK}/master/hr/department/${id}`;
+        try {
+            const res = await axios.put(url,
+                {
+                    nama_department: namaDepartmentEdit
+
+                },
+                {
+
+                    withCredentials: true,
+                });
+            window.location.reload();
+            getKaryawan()
+            console.log(res.data);
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+    async function deleteMasterMesin(id: number) {
+
+        const url = `${import.meta.env.VITE_API_LINK}/master/hr/department/${id}`;
+        try {
+            const res = await axios.delete(url,
+                {
+
+                    withCredentials: true,
+                });
+            window.location.reload();
+            getKaryawan()
+            console.log(res.data);
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+
+    const [showDelete, setShowDelete] = useState<any>([]);
+    const openDelete = (i: any) => {
+        const onchangeVal: any = [...showDelete];
+        onchangeVal[i] = true;
+
+        setShowDelete(onchangeVal);
+    };
+    const closeDelete = (i: any) => {
+        const onchangeVal: any = [...showDelete];
+        onchangeVal[i] = false;
+
+        setShowDelete(onchangeVal);
+    };
     return (
         <div>
             <>
@@ -77,9 +133,58 @@ function IsiDepartment() {
                         <div className='flex w-full  pr-8 border-b-8 border-[#D8EAFF] pb-2'>
                             <div className='px-2 py-1 flex w-full justify-end items-center'>
                                 <button
+                                    onClick={() => openModalHistory()}
                                     className=' bg-blue-600 rounded-sm text-white text-xs font-bold px-4 py-2'>
                                     TAMBAH DEPARTMENT
                                 </button>
+                                {showHistory == true && (
+                                    <>
+                                        <ModalKosonganSmall
+                                            isOpen={showHistory}
+                                            onClose={() => closeModalHistory()}
+                                            judul={'Tambah Department'}
+                                        >
+                                            <>
+                                                <div className="grid   gap-3 w-full px-5 py-2">
+                                                    <>
+                                                        <form onSubmit={(e) => {
+                                                            e.preventDefault()
+                                                            postMasterMesin()
+                                                        }}>
+                                                            <div className="flex w-full flex-col">
+                                                                <label className="text-black text-xs font-bold">
+                                                                    Nama Department
+                                                                </label>
+                                                                <div className="flex w-full">
+                                                                    <input
+                                                                        required
+                                                                        name="nama_department"
+                                                                        onChange={(e) => { setnamaDepartment(e.target.value) }}
+                                                                        type="text"
+                                                                        className=" w-[387px] h-10 border-2 border-stroke rounded-md"
+                                                                    />
+                                                                </div>
+
+                                                            </div>
+
+
+                                                            <div className=" pt-3">
+                                                                <button
+                                                                    type='submit'
+                                                                    value='submit'
+                                                                    className="bg-[#0065DE] text-center text-white text-xs font-bold px-6 py-3 rounded-md"
+                                                                >
+                                                                    SIMPAN
+                                                                </button>
+                                                            </div>
+                                                        </form>
+                                                    </>
+                                                </div>
+                                            </>
+                                        </ModalKosonganSmall>
+                                    </>
+                                )
+                                }
                             </div>
                         </div>
 
@@ -103,12 +208,85 @@ function IsiDepartment() {
                                             <label className="text-neutral-500 text-xs font-semibold  ">
                                                 {i + 1}
                                             </label>
-                                            <label className="text-neutral-500 text-xs font-semibold  col-span-8">
+                                            <label className="text-neutral-500 text-xs font-semibold  col-span-7">
                                                 {data.nama_department}
                                             </label>
-                                            <button className='bg-blue-600 rounded-sm text-white text-xs font-bold px-4 py-1'>
+                                            <button
+                                                onClick={() => openEdit(i)}
+                                                className='bg-blue-600 rounded-sm text-white text-xs font-bold px-4 py-1'>
                                                 EDIT
                                             </button>
+                                            {showEdit[i] == true && (
+
+                                                <ModalKosonganSmall
+                                                    isOpen={showEdit[i]}
+                                                    onClose={() => closeEdit(i)}
+                                                    judul={'Edit Department'}
+                                                >
+                                                    <>
+                                                        <div className="grid   gap-3 w-full px-5 py-2">
+                                                            <>
+
+                                                                <div className="flex w-full flex-col">
+                                                                    <label className="text-black text-xs font-bold">
+                                                                        Nama Department
+                                                                    </label>
+                                                                    <div className="flex w-full">
+                                                                        <input
+                                                                            name="nama_department"
+                                                                            defaultValue={data.nama_department}
+                                                                            onChange={(e) => { setnamaDepartmentEdit(e.target.value) }}
+                                                                            type="text"
+                                                                            className=" w-[387px] h-10 border-2 border-stroke rounded-md"
+                                                                        />
+                                                                    </div>
+
+                                                                </div>
+
+                                                                <div className=" pt-3">
+                                                                    <button
+                                                                        onClick={() => editMasterMesin(data.id)}
+                                                                        className="bg-[#0065DE] text-center text-white text-xs font-bold px-6 py-3 rounded-md"
+                                                                    >
+                                                                        SIMPAN
+                                                                    </button>
+                                                                </div>
+
+                                                            </>
+                                                        </div>
+                                                    </>
+                                                </ModalKosonganSmall>
+                                            )}
+                                            <button
+                                                onClick={() => openDelete(i)}
+                                                className='bg-red-600 rounded-sm text-white text-xs font-bold px-4 py-1'>
+                                                DELETE
+                                            </button>
+                                            {showDelete[i] == true && (
+                                                <>
+                                                    <ModalKosonganSmall
+                                                        isOpen={showDelete[i]}
+                                                        onClose={() => closeDelete(i)}
+                                                        judul={'Hapus Department'}
+                                                    >
+                                                        <>
+                                                            <div className="flex w-full flex-col pt-7 px-2 py-3 justify-center items-center">
+                                                                <>
+                                                                    <label className="text-black text-xl font-bold">
+                                                                        {data.nama_department}
+                                                                    </label>
+                                                                    <button
+                                                                        onClick={() => deleteMasterMesin(data.id)}
+                                                                        className='bg-red-600 h-7 w-full rounded-md text-white text-xs font-bold px-4 py-1'>
+                                                                        HAPUS DEPARTMENT
+                                                                    </button>
+                                                                </>
+                                                            </div>
+                                                        </>
+                                                    </ModalKosonganSmall>
+                                                </>
+                                            )
+                                            }
                                         </div>
                                     </>
                                 ))}
