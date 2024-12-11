3f2227958e3f6162112842b36e607e7115fff5f8
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-12-11 15:46:38 +0700

    update

--- src/components/Tables/HR/MasterData/MasterShiftHRD.tsx
+++ src/components/Tables/HR/MasterData/MasterShiftHRD.tsx
@@ -64,6 +64,13 @@ function MasterShiftHRD() {
 
         setShowModal(onchangeVal);
     };
+
+    const [showModal1, setShowModal1] = useState(false);
+
+    const openModal1 = () => setShowModal1(true);
+    const closeModal1 = () => setShowModal1(false);
+
+
     const [shift1MasukEdit, setShift1MasukEdit] = useState<any>();
     const [shift1KeluarEdit, setShift1KeluarEdit] = useState<any>();
     const [shift2MasukEdit, setShift2MasukEdit] = useState<any>();
@@ -95,60 +102,215 @@ function MasterShiftHRD() {
             //alert(error.data.msg);
         }
     }
+    const [istirahat, setIstirahat] = useState([
+        {
+            id_shift: '',
+            dari: '',
+            sampai: '',
+            nama: '',
+
+        },
+    ]);
+    //add Point
+    const handleAddPoint = () => {
+        setIstirahat([
+            ...istirahat,
+            {
+                id_shift: '',
+                dari: '',
+                sampai: '',
+                nama: '',
+            },
+        ]);
+    };
+    const handleChangePointTask = (e: any, i: number) => {
+        const { name, value } = e.target;
+        const onchangeVal: any = [...istirahat];
+        onchangeVal[i][name] = value;
+        setIstirahat(onchangeVal);
+    };
+
+    const handleChangeIDShift = (e: any, i: number) => {
+        const onchangeVal: any = [...istirahat];
+        onchangeVal[i]['id_shift'] = e;
+        setIstirahat(onchangeVal);
+        console.log(onchangeVal)
+    };
+
+    const handleDeletePoint = (i: number) => {
+        const deleteVal: any = [...istirahat];
+        deleteVal.splice(i, 1);
+        setIstirahat(deleteVal);
+    };
     return (
         <>
 
             <main className="overflow-x-scroll">
                 <div className="min-w-[700px] bg-white rounded-xl">
                     <div className=" w-full h-full flex-col border-b-8 border-[#D8EAFF]">
-                        <div className="grid grid-cols-12 px-10 py-4 border-b-8 border-[#D8EAFF] gap-2 ">
-                            <label className="text-neutral-500 text-sm font-semibold ">
-                                No
-                            </label>
+                        <div className="grid grid-cols-8 px-10 py-4 border-b-8 border-[#D8EAFF] gap-2 ">
+                            <div className='flex gap-1'>
 
-                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
-                                Hari
-                            </label>
-                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                                <label className="text-neutral-500 text-sm font-semibold ">
+                                    No
+                                </label>
+                                <label className="text-neutral-500 text-sm font-semibold ">
+                                    Hari
+                                </label>
+                            </div>
+                            <label className="text-neutral-500 text-sm font-semibold ">
                                 Shift 1 Masuk
                             </label>
-                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                            <label className="text-neutral-500 text-sm font-semibold ">
                                 Shift 1 Keluar
                             </label>
-                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                            <label className="text-neutral-500 text-sm font-semibold ">
                                 Shift 2 Masuk
                             </label>
-                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                            <label className="text-neutral-500 text-sm font-semibold ">
                                 Shift 2 Keluar
                             </label>
+                            <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                                Jam Istirahat
+                            </label>
+                            <div>
+                                <>
+
+                                    <button
+                                        onClick={() => {
+
+                                            openModal1()
+                                        }}
+                                        className={`uppercase px-3 inline-flex rounded-[3px] items-center text-white text-xs font-bold  py-2 my-2   hover:bg-blue-400 border bg-blue-600 border-blue-600  justify-center`} // Dynamic class assignment
+                                    >
+                                        Tambah Jam Istirahat
+                                    </button>
+                                    {showModal1 && (
+                                        <>
+                                            <ModalKosonganSmall
+                                                isOpen={showModal1}
+                                                onClose={closeModal1}
+                                                judul={'Tambah Jam Istirahat Shift'}>
+                                                <>
+                                                    <div className='flex flex-col gap-1 px-4 py-1'>
+
+
+                                                        <button
+                                                            onClick={handleAddPoint}
+                                                            className=" w-[45%] h-8 rounded-md  bg-blue-600 text-white text-sm font-bold justify-center items-center  hover:cursor-pointer"
+                                                        >
+                                                            +
+                                                        </button>
+                                                        {
+                                                            istirahat?.map((data3: any, iii: any) => {
+
+                                                                return (
+                                                                    <>
+                                                                        <div className="flex w-full flex-col">
+                                                                            <label className="text-black text-xs font-bold">
+                                                                                Nama
+                                                                            </label>
+                                                                            <div className="flex w-full">
+                                                                                <input
+                                                                                    name="nama"
+                                                                                    //onChange={(e) => { setShift1MasukEdit(e.target.value) }}
+                                                                                    type="text"
+                                                                                    className=" w-[387px] h-10 border-2 border-stroke rounded-md"
+                                                                                />
+                                                                            </div>
+
+                                                                        </div>
+                                                                        <div className="flex w-full flex-col">
+                                                                            <label className="text-black text-xs font-bold">
+                                                                                Jam Masuk
+                                                                            </label>
+                                                                            <div className="flex w-full">
+                                                                                <input
+                                                                                    name="dari"
+                                                                                    //onChange={(e) => { setShift1MasukEdit(e.target.value) }}
+                                                                                    type="time"
+                                                                                    className=" w-[387px] h-10 border-2 border-stroke rounded-md"
+                                                                                />
+                                                                            </div>
+
+                                                                        </div>
+                                                                        <div className="flex w-full flex-col">
+                                                                            <label className="text-black text-xs font-bold">
+                                                                                Jam Keluar
+                                                                            </label>
+                                                                            <div className="flex w-full">
+                                                                                <input
+                                                                                    name="sampai"
+                                                                                    //onChange={(e) => { setShift1MasukEdit(e.target.value) }}
+                                                                                    type="time"
+                                                                                    className=" w-[387px] h-10 border-2 border-stroke rounded-md"
+                                                                                />
+                                                                            </div>
+
+                                                                        </div>
+                                                                        <button
+                                                                            onClick={() => handleDeletePoint(iii)}
+                                                                            className=" w-[45%]  h-8 rounded-md bg-red-500 text-white text-sm font-bold justify-center items-center  hover:cursor-pointer"
+                                                                        >
+                                                                            HAPUS
+                                                                        </button>
+                                                                    </>
+                                                                )
+                                                            }
+                                                            )}
+                                                    </div>
+                                                </>
+                                            </ModalKosonganSmall>
+                                        </>
+                                    )
+                                    }
+                                </>
+
+                            </div>
                         </div>
                         <div className="w-2 h-full "></div>
                         {cetakMesin?.data.map((data: any, i: any) => {
-
                             return (
                                 <>
-                                    <div className="grid grid-cols-12 px-10 items-center border-b-8 border-[#D8EAFF] gap-2 ">
-                                        <label className="text-neutral-500 text-sm font-semibold ">
-                                            {i + 1}
-                                        </label>
-
-                                        <label className="text-neutral-500 text-sm font-semibold col-span-2">
-                                            {data.hari}
-                                        </label>
-                                        <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                                    <div className="grid grid-cols-8 px-10 items-center border-b-8 border-[#D8EAFF] gap-2 ">
+                                        <div className='flex gap-1'>
+                                            <label className="text-neutral-500 text-sm font-semibold ">
+                                                {i + 1}.
+                                            </label>
+                                            <label className="text-neutral-500 text-sm font-semibold ">
+                                                {data.hari}
+                                            </label>
+                                        </div>
+                                        <label className="text-neutral-500 text-sm font-semibold">
                                             {data.shift_1_masuk}
                                         </label>
-                                        <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                                        <label className="text-neutral-500 text-sm font-semibold">
                                             {data.shift_1_keluar}
                                         </label>
-                                        <label className="text-neutral-500 text-sm font-semibold col-span-2">
+                                        <label className="text-neutral-500 text-sm font-semibold">
                                             {data.shift_2_masuk}
                                         </label>
-                                        <label className="text-neutral-500 text-sm font-semibold col-span-2">
+
+                                        <label className="text-neutral-500 text-sm font-semibold ">
                                             {data.shift_2_keluar}
                                         </label>
+                                        <div className='flex flex-col gap-1 col-span-2'>
+                                            {
+                                                data?.istirahat?.map((data2: any, ii: any) => {
 
-                                        <div className=" flex ">
+                                                    return (
+                                                        <>
+                                                            <label className="text-neutral-500 text-sm font-semibold ">
+                                                                {ii + 1}. {data2.dari} -  {data2.sampai}
+                                                            </label>
+                                                        </>
+                                                    )
+                                                }
+                                                )
+                                            }
+                                        </div >
+
+                                        <div className=" flex gap-1">
                                             <>
 
                                                 <button
@@ -168,7 +330,6 @@ function MasterShiftHRD() {
                                                                     <label className="text-black text-sm font-bold">
                                                                         Hari : {data.hari}
                                                                     </label>
-
                                                                     <div className="flex w-full flex-col">
                                                                         <label className="text-black text-xs font-bold">
                                                                             Shift 1 Masuk
@@ -246,12 +407,13 @@ function MasterShiftHRD() {
                                                 )
                                                 }
                                             </>
+
                                         </div>
-                                    </div>
+                                    </div >
                                 </>
                             )
                         })}
-                    </div>
+                    </div >
                 </div>
             </main>
 
