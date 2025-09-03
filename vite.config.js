750ce8b4d5debc9f472784b1ae25efce6cbe6128
Rully <mauliarahman214@gmail.com>
2025-09-04 02:29:45 +0700

    UP

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
-        assetFileNames: '[name]-[hash].[ext]',
-      },
-    },
-  },
-  assetsInclude: ['**/*.wasm'],
 });
