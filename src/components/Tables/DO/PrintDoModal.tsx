3ea6831b31e5961894fed2e937974f53be872ec9
Rully <mauliarahman214@gmail.com>
2026-02-25 13:49:47 +0700

    up date

--- src/components/Tables/DO/PrintDoModal.tsx
+++ src/components/Tables/DO/PrintDoModal.tsx
@@ -125,7 +125,7 @@ const PrintDoModal: React.FC<PrintDoModalProps> = ({
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
-    return `${day} December ${year}`;
+    return `${day} ${month} ${year}`;
   };
 
   const removeJOPrefix = (noJo: string): string => {
